361ad08be8540e2ac191d26556b7f42b32e0441a
natehopkins <nhop5040@gmail.com>
2016-11-18 17:08:29 -0700

    Got collection view working for entreeCVC
    added collection view cell, entreecollectionview controller,

---
+++ Taste/EntreeTypeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EntreeTypeCollectionViewCell.swift
+//  Taste
+//
+//  Created by Nathan Hopkins on 11/18/16.
+//  Copyright © 2016 NathanHopkins. All rights reserved.
+//
+
+import UIKit
+
+class EntreeTypeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Taste/HeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCollectionReusableView.swift
+//  Taste
+//
+//  Created by Nathan Hopkins on 11/18/16.
+//  Copyright © 2016 NathanHopkins. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+}
